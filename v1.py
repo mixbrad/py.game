cb5b22f0cc515d9ed8affd7d0d6adef9d74ff308
Diogo <Diogo.nadalini@gmail.com>
2024-05-13 14:41:41 -0300

    27:46- versao 2

--- v1.py
+++ v1.py
@@ -2,10 +2,13 @@
 import pygame
 import random
 
+#definição de variaveis pro jogo
 pygame.init()
 relogio= pygame.time.Clock()
 fps=60
-
+fonte= pygame.font.Font("freesansbold.ttf", 30)
+clock= pygame.time.Clock()
+active= False
 
 #definição das cores
 branco=(255,255,255)
@@ -15,40 +18,90 @@ vermelho=(255,0,0)
 laranja=(255,128,0)
 verde=(0,255,0)
 azul=(0,0,255)
-roxo=(238,130,238)
+roxo=(138,43,226)
 amarelo=(255,255,0)
-rosa=(255,192,203)
+rosa=(255,105,180)
 cores=[vermelho, laranja, amarelo, verde, azul, roxo, rosa ]
 
 #definição da tela e da fonte
 altura= 770
 largura= 500
-screen= pygame.display.set_mode((largura, altura))
-fonte= pygame.font.Font("freesansbold.ttf", 30)
-clock= pygame.time.Clock()
+tela= pygame.display.set_mode((largura, altura))
+#tabuleiro= [[1,1,1,1,1],[2,2,2,2,2],[3,3,3,3,3],[4,4,4,4,4],[5,5,5,5,5],[6,6,6,6,6],[7,7,7,7,7]]
+tabuleiro= [[7,7,7,7,7],[6,6,6,6,6],[5,5,5,5,5],[4,4,4,4,4],[3,3,3,3,3],[2,2,2,2,2],[1,1,1,1,1]]
+def desenhar_tabuleiro(tabuleiro):
+    tabuleiro_alvos=[]
+    for i in range(len(tabuleiro)):
+        for j in range(len(tabuleiro[i])):
+            if tabuleiro[i][j]>0:
+                alvos= pygame.draw.rect(tela, cores[(tabuleiro[i][j])-1], [j*100, i*40, 98, 38])
+                limite_alto= pygame.rect.Rect((j*100, i*40), (98, 1))
+                limite_baixo= pygame.rect.Rect((j*100, i*40)+37), (98, 1)
+                esquerda= pygame.rect.Rect((j*100, i*40), (37, 1))
+                direita= pygame.rect.Rect((j*100)+97 , i*40), (37, 1)
+                tabuleiro_alvos.append([limite_alto, limite_baixo, esquerda, direita, (i, j)])
+    return tabuleiro_alvos
 
 
 #definicão das variáveis do jogador e da bola
 jogador_x= 190
 bola_x= largura/2
 bola_y= altura-30
-tabuleiro= [[5,5,5,5,5],[4,4,4,4,4],[3,3,3,3,3],[2,2,2,2,2],[1,1,1,1,1]]
-
-def desenhar_tabuleiro(tabuleiro):
-    for i in range(len(tabuleiro)):
-        for j in range(len(tabuleiro[i])):
-            pygame.draw.rect(screen, cores[(tabuleiro[i][j])-1], [j*100, i*40, 98, 38])
+movimento_bola_x= 0
+movimento_bola_y= 0
+velocidade_bola_x= 5
+velocidade_bola_y= 5
+velocidade_jogador= 10
+movimento_jogador= 0
 
 run= True
 while run:
-    screen.fill(cinza)
+    tela.fill(cinza)
     relogio.tick(fps)
-    desenhar_tabuleiro(tabuleiro)
-    jogador= pygame.draw.rect(screen, preto, [jogador_x, altura-20, 120, 15], 0, 3)
-    bola= pygame.draw.circle(screen, branco, (bola_x, bola_y), 10)
+    alvos= desenhar_tabuleiro(tabuleiro)
+    jogador= pygame.draw.rect(tela, preto, [jogador_x, altura-20, 120, 15], 0, 3)
+    bola= pygame.draw.circle(tela, branco, (bola_x, bola_y), 10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run= False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and not active:
+                active= True 
+                movimento_bola_y= -1
+                movimento_bola_x= random.choice ([-1, 1])
+            if event.key == pygame.K_RIGHT and active:
+                movimento_jogador= 1
+            if event.key == pygame.K_LEFT and active:
+                movimento_jogador= -1
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                movimento_jogador= 0
+            if event.key == pygame.K_LEFT:
+                movimento_jogador= 0
+
+    #delimitação para a bola não sair do tabuleiro
+    if bola_x <= 10 or bola_x >= largura-10:
+        movimento_bola_x*= -1
+
+    for i in range(len(alvos)):
+        if bola.colliderect(alvos[i][0]) or bola.colliderect(alvos[i][1]):
+            movimento_bola_y*= -1
+            tabuleiro[alvos[i][1][0]][alvos[i][1][1]]-= 1
+        if bola.colliderect(alvos[i][2]) or bola.colliderect(alvos[i][1]):
+            movimento_bola_x*= -1
+            tabuleiro[alvos[i][1][0]][alvos[i][1][1]]-= 1
+
+    #delimitação para a bola não passar pelo jogador
+    if bola.colliderect(jogador):
+        movimento_bola_y*= -1
+
+    #movimento da bola
+    bola_y+= movimento_bola_y* velocidade_bola_y
+    bola_x+= movimento_bola_x* velocidade_bola_x
+    #movimento do jogador
+    jogador_x+=movimento_jogador* velocidade_jogador
+        
+
     
     pygame.display.flip()
 pygame.quit()
